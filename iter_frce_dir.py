d7e2a1bfa76ea8febace1fea21b349d469682f76
abureyanahmed <abureyanahmed@email.arizona.edu>
2020-03-21 15:21:41 -0700

    Update iter_frce_dir.py

--- iter_frce_dir.py
+++ iter_frce_dir.py
@@ -73,7 +73,8 @@ def draw_graph(x, y, edge_list, output_file, labels, label_sizes):
  #plt.close(fig)
  plt.show()
 
-def find_suitable_point(u, v_x, v_y, D_x, D_y, edge_list):
+def find_suitable_point(u, v_x, v_y, D_x, D_y, edge_list, index_to_label, label_sizes):
+ #print("find_suitable_point:", index_to_label[u])
  crossing_exist = True
  while crossing_exist:
   crossing_exist = False
@@ -84,15 +85,21 @@ def find_suitable_point(u, v_x, v_y, D_x, D_y, edge_list):
      crossing_exist = True
      break
   if not crossing_exist:
-   # future todo: check whether the edge is inside a box, then run binary search, this is harder, we don't know any valide end point
+   for i in range(len(D_x)):
+    crossing_exist = do_label_overlab_with_segment(i, D_x[i], D_y[i], D_x[u], D_y[u], v_x, v_y, index_to_label, label_sizes[i])
+    if crossing_exist:
+     break
+  if not crossing_exist:
+   # future todo: check whether the edge is inside a box, then run binary search, this is harder, we don't know any valid end point
    return v_x, v_y
   v_x = D_x[u] + (v_x - D_x[u])/2.0
   v_y = D_y[u] + (v_y - D_y[u])/2.0
 
 def find_suitable_point_scale_up(current_vertex, change_x, change_y, D_x, D_y, edge_list, label_sizes, index_to_label):
+ #print("find_suitable_point_scale_up", index_to_label[current_vertex])
  crossing_exist = True
  while crossing_exist:
-  print("change_x, change_y:",change_x, change_y)
+  #print("change_x, change_y:",change_x, change_y)
   crossing_exist = False
   for e in edge_list:
    p1, q1 = e
@@ -195,19 +202,22 @@ def find_suitable_font_size(i, v_x, v_y, D_x, D_y, edge_list, label_sizes, label
   font_size = font_size/2
 
 def find_suitable_font_size_scale_up(j, D_x, D_y, edge_list, label_sizes, index_to_label):
- d = 1
+ #d = 1
  org_font_size = label_sizes[j]
+ scale = 12/org_font_size
  loop_counter = 0
  while True:
-  scale = 1+1/d
+  #scale = 1+1/d
   loop_counter += 1
-  if (scale-1)<.001 or loop_counter>10:
-   return org_font_size
+  #if (scale-1)<.001 or loop_counter>10:
+  #if (scale-1)<.001:
+  # return org_font_size
   font_size = min(org_font_size*scale, 12)
   overlap_exist = do_overlap_exist(j, D_x[j], D_y[j], D_x, D_y, edge_list, label_sizes, index_to_label, font_size)
   if not overlap_exist:
    return font_size
-  d = 2*d
+  #d = 2*d
+  scale /= 2
 
 def iterative_force_directed(G):
  label_to_index = dict()
@@ -230,7 +240,15 @@ def iterative_force_directed(G):
  index_to_label[ind] = txt
  #for e in G.edges():
  edge_list = [[0, 1], [1, 2], [2, 3], [3, 0]]
- for e in nx.bfs_edges(G, "machine learning"):
+ #bfs_edges = []
+ #for e in nx.bfs_edges(G, "machine learning"):
+ # u, v = e
+ # bfs_edges.append((u, v))
+ #print(bfs_edges)
+ #quit()
+ bfs_edges = [('machine learning', 'computer vision'), ('machine learning', 'signal processing'), ('machine learning', 'data mining'), ('machine learning', 'natural language processing'), ('machine learning', 'optimization'), ('machine learning', 'neuroscience'), ('machine learning', 'bioinformatics'), ('machine learning', 'statistics'), ('machine learning', 'artificial intelligence'), ('computer vision', 'image processing'), ('computer vision', 'robotics'), ('neuroscience', 'psychology'), ('bioinformatics', 'genomics'), ('bioinformatics', 'computational biology'), ('bioinformatics', 'biostatistics'), ('statistics', 'econometrics'), ('artificial intelligence', 'computer science'), ('psychology', 'education'), ('genomics', 'evolution'), ('genomics', 'genetics'), ('biostatistics', 'epidemiology'), ('econometrics', 'economics'), ('computer science', 'software engineering'), ('evolution', 'ecology'), ('genetics', 'molecular biology'), ('ecology', 'conservation biology'), ('ecology', 'climate change'), ('molecular biology', 'microbiology'), ('molecular biology', 'biochemistry'), ('climate change', 'hydrology'), ('microbiology', 'immunology'), ('biochemistry', 'biophysics'), ('hydrology', 'remote sensing'), ('immunology', 'cancer'), ('cancer', 'stem cells'), ('stem cells', 'tissue engineering'), ('tissue engineering', 'biomaterials'), ('biomaterials', 'drug delivery'), ('drug delivery', 'nanotechnology'), ('nanotechnology', 'materials science'), ('materials science', 'physics'), ('materials science', 'chemistry')]
+ #for e in nx.bfs_edges(G, "machine learning"):
+ for e in bfs_edges:
   #print(e)
   u, v = e
   if not u in label_to_index.keys():
@@ -286,32 +304,37 @@ def iterative_force_directed(G):
     v_x, v_y = D_x[u] + D_y[label_to_index[N_u2[min_j]]] - D_y[label_to_index[N_u2[0]]], D_y[u] + D_x[label_to_index[N_u2[0]]] - D_x[label_to_index[N_u2[min_j]]]
    else:
     v_x, v_y = D_x[u] + D_x[label_to_index[N_u2[0]]] - D_x[label_to_index[N_u2[min_j]]] + D_x[label_to_index[N_u2[1]]] - D_x[label_to_index[N_u2[min_j]]], D_y[u] + D_y[label_to_index[N_u2[0]]] - D_y[label_to_index[N_u2[min_j]]] + D_y[label_to_index[N_u2[1]]] - D_y[label_to_index[N_u2[min_j]]]
-  v_x, v_y = find_suitable_point(u, v_x, v_y, D_x, D_y, edge_list)
+  v_x, v_y = find_suitable_point(u, v_x, v_y, D_x, D_y, edge_list, index_to_label, label_sizes)
   fnt_size = find_suitable_font_size(v, v_x, v_y, D_x, D_y, edge_list, label_sizes, index_to_label, 12)
   D_x.append(v_x)
   D_y.append(v_y)
   label_sizes.append(fnt_size)
   output_file = "iteration_"+str(i)+".png"
-  draw_graph(D_x, D_y, edge_list[:len(D_x)-1], output_file, index_to_label, label_sizes)
+  #draw_graph(D_x, D_y, edge_list[:len(D_x)-1], output_file, index_to_label, label_sizes)
   max_diff = math.sqrt(640**2 + 480**2)
+  prev_max_diff = max_diff + 20
   loop_counter = 0
-  #while max_diff>10 or abs(label_sizes[len(label_sizes)-1]-12)<.001:
-  while max_diff>10:
+  while abs(max_diff-prev_max_diff)>10 or abs(label_sizes[len(label_sizes)-1]-12)>.001:
+  #while max_diff>10 or abs(label_sizes[len(label_sizes)-1]-12)>.001:
+  #while max_diff>10:
    loop_counter += 1
-   if loop_counter>100: break
+   #if loop_counter>100: break
+   print("max_diff:", max_diff, "label_size:", label_sizes[len(label_sizes)-1])
    #print("D_x", D_x)
+   prev_max_diff = max_diff
    max_diff = 0
    for j in range(4, len(D_x)):
-    param = 5
+    param = 2*math.sqrt(640*480/i)
     repulsion_x = 0
     repulsion_y = 0
-    param_a = 5
+    #param_a = 5
+    param_a = 2*math.sqrt(640*480/i)
     attraction_x = 0
     attraction_y = 0
     for k in range(len(D_x)):
      if k==j: continue
-     print("calculating repulsion between:", index_to_label[k], k,  " and ", index_to_label[j])
-     if not D_x[j]==D_x[k]: repulsion_x += param*((D_x[j]-D_x[k])/abs(D_x[j]-D_x[k]))/abs(D_x[j]-D_x[k])
+     #print("calculating repulsion between:", index_to_label[k], k,  " and ", index_to_label[j])
+     if not D_x[j]==D_x[k]: repulsion_x += (param**2)*((D_x[j]-D_x[k])/abs(D_x[j]-D_x[k]))/abs(D_x[j]-D_x[k])
      if not D_y[j]==D_y[k]: repulsion_y += (param**2)*((D_y[j]-D_y[k])/abs(D_y[j]-D_y[k]))/abs(D_y[j]-D_y[k])
     for e in edge_list:
      u, v = e
@@ -328,8 +351,11 @@ def iterative_force_directed(G):
     if j == len(label_sizes)-1:
      fnt_size = find_suitable_font_size_scale_up(j, D_x, D_y, edge_list, label_sizes, index_to_label)
      label_sizes[len(label_sizes)-1] = fnt_size
+    max_diff = max(max_diff, math.sqrt((repulsion_x+attraction_x)**2+(repulsion_y+attraction_y)**2))
   i = i+1
-  #if i==10: break
+  if i==15: break
+ #print("D_x, D_y:", D_x, D_y)
+ draw_graph(D_x, D_y, edge_list[:len(D_x)-1], output_file, index_to_label, label_sizes)
 
 G = nx_read_dot('Layer1.dot')
 #G2 = nx.Graph()
@@ -338,4 +364,5 @@ G = nx_read_dot('Layer1.dot')
 #plt.show()
 
 iterative_force_directed(G)
+
 
